48e47c23e7d520716eecd62fdbca745b838a609b
EvertonCa <eve.023@hotmail.com>
2018-03-03 10:23:33 -0300

    Upload de novos codigos

---
+++ src/Aula4Ex3Pessoa.java
@@ -0,0 +1,30 @@
+public class Aula4Ex3Pessoa
+{
+    public void setCPF (String cpf)
+    {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public String getNome()
+    {
+        return nome;
+    }
+
+    public String getCPF()
+    {
+        return cpf;
+    }
+
+    private String nome;
+    private String cpf;
+
+}
+
+
+
+
